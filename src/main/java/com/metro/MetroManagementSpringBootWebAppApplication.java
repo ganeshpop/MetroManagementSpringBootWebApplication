7588bac0a8f47ea15de43abba02201f51f9dda69
Ganesh <ganeshgo1999@gmail.com>
2021-10-04 10:18:13 +0530

    Kafka - Logging Users and Transactions

--- src/main/java/com/metro/MetroManagementSpringBootWebAppApplication.java
+++ src/main/java/com/metro/MetroManagementSpringBootWebAppApplication.java
@@ -2,12 +2,17 @@ package com.metro;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(scanBasePackages = "com.metro")
 public class MetroManagementSpringBootWebAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MetroManagementSpringBootWebAppApplication.class, args);
+        ApplicationContext applicationContext = SpringApplication.run(MetroManagementSpringBootWebAppApplication.class, args);
+        for(String name: applicationContext.getBeanDefinitionNames()){
+            System.out.println("--> " + name );
+        }
+
     }
 
 }
